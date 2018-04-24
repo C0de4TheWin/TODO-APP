048c3c2d086577758080cf475a35c9e581e1c091
Sam De Legge <samd@Sams-MacBook-Pro.local>
2018-04-24 11:41:36 -0700

    final update, app works

---
+++ routes/api-routes.js
@@ -0,0 +1,64 @@
+// *********************************************************************************
+// api-routes.js - this file offers a set of routes for displaying and saving data to the db
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+
+// Requiring our Todo model
+var db = require("../models");
+
+// Routes
+// =============================================================
+module.exports = function (app) {
+
+  // GET route for getting all of the todos
+  app.get("/api/todos", function (req, res) {
+    db.Todo.findAll({}).then(function (results) {
+      res.json(results);
+    });
+  });
+
+  // POST route for saving a new todo. We can create todo with the data in req.body
+  app.post("/api/todos", function (req, res) {
+
+    db.Todo.create({
+      text: req.body.text,
+      complete: false,
+    }).then(function (data) {
+      res.json(data);
+    });
+
+  });
+
+  // DELETE route for deleting todos. We can get the id of the todo to be deleted from
+  // req.params.id
+  app.delete("/api/todos/:id", function (req, res) {
+
+    db.Todo.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(function (data) {
+      res.end()
+
+    })
+
+
+  });
+
+  // PUT route for updating todos. We can get the updated todo data from req.body
+  app.put("/api/todos", function (req, res) {
+
+    db.Todo.update({
+      complete: true
+    },
+      { where: { id: req.body.id } }
+    )
+
+      .then(function () {
+        res.end();
+      })
+
+  })
+};
